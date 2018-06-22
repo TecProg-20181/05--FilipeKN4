34f22d581168f9d60a0279125fddf4fad4bd009f
FilipeKN4 <filipekn4@gmail.com>
2018-06-21 21:53:25 -0300

    Create a test for bytes_to_readable

--- diskspace/tests.py
+++ diskspace/tests.py
@@ -1,10 +1,23 @@
-from diskspace import *
+import diskspace
 import unittest
+import subprocess
+import argparse
+import os
+import re
 
 class TestDiskspace(unittest.TestCase):
 
     def test_subprocess_check_output(self):
         command = 'du -d 1 /home/filipe/Documentos'
-        command = command.strip().split(' ')
-        result = ['du', '-d', '1', '/home/filipe/Documentos']
-        self.assertEqual(command, result)
+        result = diskspace.subprocess_check_output(command)
+        test = subprocess.check_output(command.strip().split(' '))
+        self.assertEqual(result, test)
+
+    def test_bytes_to_readable(self):
+        blocks = 304
+        result = diskspace.bytes_to_readable(blocks)
+        test = '152.00Kb'
+        self.assertEqual(result, test)
+
+if __name__ == '__main__':
+    unittest.main()
